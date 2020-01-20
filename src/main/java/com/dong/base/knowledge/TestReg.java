2f1d625f5f0ed16d327c57e2e2e2372becafa73a
dongjunpeng <junpeng.dong@causacloud.com>
2020-01-20 19:01:01 +0800

    缓存数据库

--- src/main/java/com/dong/base/knowledge/TestReg.java
+++ src/main/java/com/dong/base/knowledge/TestReg.java
@@ -39,21 +39,51 @@ public class TestReg {
 
         // 现在创建 matcher 对象
         Matcher m = r.matcher(line);
-        if (m.find( )) {
+        while (m.find( )) {
             System.out.println("Found value: " + m.group(0) );
             System.out.println("Found value: " + m.group(1) );
             System.out.println("Found value: " + m.group(2) );
             System.out.println("Found value: " + m.group(3) );
-        } else {
-            System.out.println("NO MATCH");
         }
 
         System.out.println("--------------------------------------------------------------------------------");
-        String ss ="18501033065";
-        final String REGEX_MOBILE = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
-        System.out.println(ss.matches("\\d{11}"));
-        System.out.println(ss.matches(REGEX_MOBILE));
-        System.out.println("1890000222".matches("^189.*$"));
+//        String ss ="18501033065";
+//        final String REGEX_MOBILE = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+//        System.out.println(ss.matches("\\d{11}"));
+//        System.out.println(ss.matches(REGEX_MOBILE));
+//        System.out.println("1890000222".matches("^189.*$"));
     }
+
+    @Test
+    public void test2(){
+
+        String s = "abc-123!abc-456!";
+        String regex = "[\\d]+(!)";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s);
+        while (m.find()){
+            System.out.println("---------------------------------------");
+//            String s1 = m.group(0);   //  正则表达式中匹配到的所有内容
+//            String s2 = m.group(1);   //  正则表达式中括号内匹配到的内容
+//            System.out.println(s1);
+//            System.out.println(s2);
+            for(int i=0;i<=m.groupCount();i++){
+                System.out.println(m.group(i));
+            }
+        }
+
+       /* Pattern p=Pattern.compile("\\d+");
+        Matcher m=p.matcher("我的QQ是:456456 我的电话是:0532214 我的邮箱是:aaa123@aaa.com");
+        while(m.find()) {
+            System.out.println("---------------------------");
+//            System.out.println(m.group());
+//            System.out.println(m.group(0));
+            for(int i=0;i<=m.groupCount();i++){
+                System.out.println(m.group(i));
+            }
+
+        }*/
+    }
+
 
 }
