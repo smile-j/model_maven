527f56aa54dbc7fe66e4502c189652e13613cc2d
dongjunpeng <dongjunpeng@longfor.com>
2021-06-23 17:33:49 +0800

    1

---
+++ src/main/java/com/dong/base/test/mq/Provider.java
@@ -0,0 +1,17 @@
+package com.dong.base.test.mq;
+
+/**
+ * @author dongjunpeng
+ * @Description
+ * @date 2021/6/21
+ */
+public class Provider {
+
+
+    public void sendMeg(){
+
+//        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+    }
+
+}
