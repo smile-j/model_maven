b0928233590cd0baed534bda4599061488c2eea0
dongjunpeng <dongjunpeng@huan.tv>
2022-10-21 08:47:50 +0800

    nio

---
+++ src/main/java/com/dong/Node.java
@@ -0,0 +1,17 @@
+package com.dong;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Node {
+    private int value;
+    private Node next;
+
+}
